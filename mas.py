0f903f7510d3c35f9d663a44cb7caa179f63e85c
shamshod2010 <shokirovshamshodbek71@gmail.com>
2025-01-15 16:02:55 +0500

    Add files via upload

---
+++ mas.py
@@ -0,0 +1,185 @@
+# from random import randint
+# i=randint(1,1000)
+# while True:
+#     son=int(input('son kiriting'))
+#     if son==i:
+#        print('togri',i)
+#        break
+#     elif son>i:
+#        print('kichikroq son kirting:')
+#     elif son<i:
+#        print('kattaroq son kriting:')
+        
+        
+# foydalanuvchilar=int(input('yoshingizni kiriting'))
+# if foydalanuvchilar<7:
+#    print('sz ucun bepul')
+# elif foydalanuvchilar<16:
+#    print('5000')
+# else:
+#    print(2000)
+ 
+# son=0
+# while True:
+#     i=int(input())
+#     son=son+i
+#     if i==0:
+#        print(son) 
+#        break
+      
+      
+# n=int(input('narx'))
+# a=int(input('narx kiriting'))
+# b=int(input('narx kiritng'))
+# c=int(input('narx kiriting'))
+# if a+b+c>= n:
+#  print('yes')      
+# else:
+#  print('no')
+# n=int(input())
+# if n==1:
+#  print('bir')
+# elif n==2:
+#   print('ikki')
+# elif n==3:
+#   print('uch')
+# elif n==4:
+#   print('tort') 
+# elif n==5:
+#   print('besh')
+# elif n==6:
+#  print('olti')
+# elif n==7:
+#    print('yetti')
+# elif n==8:
+#    print('sakkiz')
+# elif n==9:
+#   print('toqqiz')
+# elif n==10:
+#    print('on')
+  
+# n=int(input('son kritng'))
+# k=int(input('son kiriting'))
+# q=k%n
+# b=k-q
+# print(int(b/n))
+  
+# a=int(input('son kiriting'))
+# b=a/10
+# print(b)
+# a=int(input('son kiriting'))
+# if a<10:
+#   print(a/10)
+# elif a<100:
+#  print(a/100)
+# elif a<1000:
+#  print(a/1000)
+ 
+# a=int(input('son kiriting'))
+# p=4*a
+# print(p)
+
+# a=int(input('son kiriting'))
+
+# a=int(input('vagonlar'))
+# if 2<=a<7:
+#  print('ekonom')
+# elif 8>=a>=9:
+#   print('biznes')
+# else:
+
+
+#  print('vip')
+  
+# def yosh(yil):
+#   yosh=2024-yil
+#   print('sz',yosh,'yoshdasz')
+# yosh(2010)
+
+# def nom (x,y):
+#   print(x**y)
+# nom(7,2)
+
+# def ab(c,d):
+#   if c>d:
+#     print(c)
+#   elif d>c:
+#     print(d)
+#   else:
+#     print('sonlar teng')
+# ab(14,10)
+    
+# mas=[1,2,3,4,5,6]
+# max_son = max(mas)
+# mas.remove(max_son)
+# max_son = max(mas)
+# # print(max_son)
+
+# yil=int(input('yil kiriting'))
+# if yil%400==0:
+#   print('kabisa yili ')
+# elif yil%4==0 and yil%100!=0:
+#   print('kabisa yili ')
+# else:
+#   print('kabisa yil emas ')
+
+# x=input()
+# if x==6:
+#   print(1)
+# elif x==5:
+#   print(2)
+# elif x==4:
+#   print(3)
+# elif x==3:
+#   print(4)
+# elif x==2:
+#   print(5)
+# else:
+#   print(6)
+
+# string='shamshod shokirov'
+# list=string.split()
+# print(list[0])
+# print(list[1]) 
+ 
+#  17.12.2024.yl
+ 
+#  print(5 in [2, 3, 5])
+# print(5 not in[2, 3, 6])
+# print([2, 4] + [5, 3] == [2, 4, 5, 3])
+#  print([2, 3] * 3 == [2, 3, 2, 3, 2, 3,])
+# print([2, 3, 7][0] == 2)
+# print([2, 3, 7][-1] == 7)
+# a = [2, 3, 7]
+# print(a[1:3:1])
+    # 18.12.2024
+# s = input()
+# if s.startswith('salom'):
+#     print('va-alekum salom')
+# else:
+#     print('oldin salom ber mol')+
+
+# a=input()
+# for i in range(0,len(a)):
+#     print(a[i])
+
+# s = input()
+# for i in s:
+#     print(i)
+
+# s=input()
+# print(s.isdigit())
+# a=input()
+# print(a.isalpha())
+# b=input()                              
+# print(b.isalnum())  
+
+# car_0 = {'model':'ferrri','rangi':'qizil'}                                                                  
+# print(car_0['rangi'])                           
+                                                                      
+# malumot = {'ismi':'shamshodbek','yosh':15,'jinsi':'erkak','yashash_joyi':'urganch'}
+# malumot['maktab']=25    
+# malumot['yosh']=16                                                           
+# print(malumot['yosh'])
+
+
